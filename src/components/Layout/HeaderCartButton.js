9138ca0a3d9cab8efe06b0ea698fb5896fc522e5
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-12-01 13:38:34 +0530

    Using Context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,23 @@
-import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
 
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numOfCartItems = cartCtx.items.reduce((curr, item) => {
+    return curr + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numOfCartItems}</span>
     </button>
   );
 };
